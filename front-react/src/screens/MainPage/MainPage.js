4b293d2bb5d9ec06e2a79f26d30a6b810e0f3ee5
vlad <vladiachuk@gmail.com>
2021-11-26 14:18:33 +0100

    Specialists table

--- front-react/src/screens/MainPage/MainPage.js
+++ front-react/src/screens/MainPage/MainPage.js
@@ -1,7 +1,10 @@
 import React from "react";
+import Specialists from "../../components/specialists/Specialists";
 
 const MainPage = () => {
-  return <div></div>;
+  return (<div>
+    <Specialists/>
+  </div>);
 };
 
 export default MainPage;
